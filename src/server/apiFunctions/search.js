b0b95af528cadd8ed04dfcf4b09323b834cd9c7f
Kevin Huang <kevinzhuang10@gmail.com>
2018-02-03 15:13:18 -0800

    complete return object config

--- src/server/apiFunctions/search.js
+++ src/server/apiFunctions/search.js
@@ -6,22 +6,23 @@ const countrySet = 'US';
 const lat = '37.773972'; // default search center lat
 const lon = '-122.431297'; // default search center lon
 
-module.exports.fuzzySearch = (req, res) => {
-  var {query} = req.body;
+module.exports = (req, res) => {
+  var {query} = req.query;
   var requestURL = `https://api.tomtom.com/search/2/search/${query}.json?key=${apiKey}&typeahead=${typeahead}&limit=${limit}&countrySet=${countrySet}&lat=${lat}&lon=${lon}`;
 
   axios.get(requestURL)
     .then(result => {
-      var places = result.map((address) => {
+      var places = result.data.results.map((place) => {
         return {
-          freeformAddress: address.freeformAddress,
-          lat: address.position.lat,
-          lon: address.position.lon
+          freeformAddress: place.address.freeformAddress,
+          lat: place.position.lat,
+          lon: place.position.lon
         };
       });
       res.status(200).send(places);
     })
     .catch(err => {
       res.status(400).send(err);
-    })
+    });
+
 }
