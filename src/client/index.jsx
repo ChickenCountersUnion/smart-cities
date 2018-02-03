09905b5f3888351eb6c82667626958c6d07b03ab
kiyeonpark <kiyeon.c.park@gmail.com>
2018-02-03 12:35:20 -0800

    add hot module conditional

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
+import { AppContainer } from 'react-hot-loader'; 
 import App from './app'; 
 
-const Index = () => {
-  return (
-    <App></App>
-  )
-}  
+const Index = (
+    <AppContainer>
+       <App/>
+    </AppContainer>
+); 
+
 
 ReactDOM.render(
-  <Index />, document.getElementById('app')
+  Index, document.getElementById('app')
 );
+
+if (module.hot) {
+  module.hot.accept(); 
+}
