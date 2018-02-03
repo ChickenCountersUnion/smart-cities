1b01ea472aa944dcc81317439f86cfb61a633013
Ian Mobley <ianmobley@gmail.com>
2018-02-03 14:24:55 -0800

    add in main route and template
    for now webpack won't chunk anything, and the manifest file isn't really
    necessary. Oh well.

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -1,10 +1,16 @@
+const path = require('path');
+const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
+const staticPath = process.env.STATIC_PATH || path.join(__dirname, '../../../dist');
 
 module.exports = (app) => {
   app.use(morgan('dev'));
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: false}));
+
+  app.use(express.static(staticPath));
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const applyMiddleware = require('./middleware');
+const routes = require('./routes');
 
 module.exports = (app) => {
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, './views'));
+
   // Pass off to middleware
   applyMiddleware(app);
 
-  // Pass off to routing
+  // Add in routes
+  app.use(routes);
 };
