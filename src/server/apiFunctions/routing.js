e7824b77ddf2b6ec99b512eb7df53ead7e301508
Kevin Huang <kevinzhuang10@gmail.com>
2018-02-03 18:07:57 -0800

    mock dummy parameters for testing

--- src/server/apiFunctions/routing.js
+++ src/server/apiFunctions/routing.js
@@ -7,31 +7,41 @@ const routeRepresentation = 'polyline';
 
 
 module.exports = (req, res) => {
-  var {query} = req.query;
-  var locations = '52.50931,13.42936:52.50274,13.43872';
+  var locations = '';
 
   // driver location as starting point
   var driverLatLon = {
-    lat: 37.773972, 
-    lon: -122.431297
+    lat: 37.7637942, 
+    lon: -122.4690077
   };
 
   var destinationLatLon = {
-    lat: 37.773972, 
-    lon: -122.431297
+    lat: 37.7086139, 
+    lon: -122.4004984
   };
 
   var driverLatLonString = createStringFromLatLon(driverLatLon);
   var destinationLatLonString = createStringFromLatLon(destinationLatLon);
   
   // remaining location , get from db
+  var wayPoints = [
+    {lat: 37.773972, lon: -122.431297},
+    {lat: 37.7897259, lon: -122.3949751},
+  ];
+
+  var wayPointsString = wayPoints.map(point => {
+    return `${point.lat},${point.lon}`;
+  }).join(':');
+
+  locations = `${driverLatLonString}:${wayPointsString}:${destinationLatLonString}`;
+
   
   //user that just submitted location get new routes
 
   //user that did not just submit poll from server
 
 
-  var requestURL = `https://api.tomtom.com/routing/1/calculateRoute/${location}?
+  var requestURL = `https://api.tomtom.com/routing/1/calculateRoute/${locations}?
     key=${apiKey}
     &alternativeType=${alternativeType}
     &instructionsType=${instructionsType}
@@ -40,14 +50,7 @@ module.exports = (req, res) => {
 
   axios.get(requestURL)
     .then(result => {
-      var places = result.data.results.map((place) => {
-        return {
-          freeformAddress: place.address.freeformAddress,
-          lat: place.position.lat,
-          lon: place.position.lon
-        };
-      });
-      res.status(200).send(places);
+      res.status(200).send(result);
     })
     .catch(err => {
       console.log(err);
