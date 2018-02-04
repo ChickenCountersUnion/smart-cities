44a01514c645e99d552cc17baebdd2bde20c6e05
kiyeonpark <kiyeon.c.park@gmail.com>
2018-02-04 10:34:06 -0800

    component for about us

---
+++ src/client/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import { Grid } from 'semantic-ui-react'; 
+
+const About = () => {
+  // <Grid celled>
+  //   <Grid.Row>
+  //     <Grid.Column width={3}>
+  //     </Grid.Column>
+  //   </Grid.Row>
+  // </Grid>
+  <div>in progress</div>
+}
+
+export default About; 
