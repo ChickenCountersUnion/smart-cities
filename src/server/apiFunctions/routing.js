c3064ec9a43b426ae72daf3a16052c54b7ff4eca
Kevin Huang <kevinzhuang10@gmail.com>
2018-02-03 19:20:39 -0800

    finish dummy implementation for routing function

--- src/server/apiFunctions/routing.js
+++ src/server/apiFunctions/routing.js
@@ -1,7 +1,5 @@
 const axios = require('axios');
 const apiKey = process.env.tomtomapikey;
-const alternativeType = 'betterRoute';
-const instructionsType = 'text';
 const computeBestOrder = true;
 const routeRepresentation = 'polyline';
 
@@ -9,53 +7,49 @@ const routeRepresentation = 'polyline';
 module.exports = (req, res) => {
   var locations = '';
 
-  // driver location as starting point
+  // DUMMY driver location as starting point
+  // san tung
   var driverLatLon = {
     lat: 37.7637942, 
     lon: -122.4690077
   };
 
+  // DUMMY recylce facility
   var destinationLatLon = {
     lat: 37.7086139, 
     lon: -122.4004984
   };
 
-  var driverLatLonString = createStringFromLatLon(driverLatLon);
-  var destinationLatLonString = createStringFromLatLon(destinationLatLon);
-  
-  // remaining location , get from db
+  // DUMMY remaining location , get from db during actual operation
   var wayPoints = [
-    {lat: 37.773972, lon: -122.431297},
     {lat: 37.7897259, lon: -122.3949751},
+    {lat: 37.773972, lon: -122.431297},
+    {lat: 37.783697, lon: -122.408966},
   ];
-
+  
   var wayPointsString = wayPoints.map(point => {
     return `${point.lat},${point.lon}`;
   }).join(':');
+  
+  var driverLatLonString = createStringFromLatLon(driverLatLon);
+  var destinationLatLonString = createStringFromLatLon(destinationLatLon);
 
   locations = `${driverLatLonString}:${wayPointsString}:${destinationLatLonString}`;
 
   
   //user that just submitted location get new routes
-
   //user that did not just submit poll from server
 
 
-  var requestURL = `https://api.tomtom.com/routing/1/calculateRoute/${locations}?
-    key=${apiKey}
-    &alternativeType=${alternativeType}
-    &instructionsType=${instructionsType}
-    &computeBestOrder=${computeBestOrder}
-    &routeRepresentation=${routeRepresentation}`;
+  var requestURL = `https://api.tomtom.com/routing/1/calculateRoute/${locations}/json?key=${apiKey}&computeBestOrder=${computeBestOrder}&routeRepresentation=${routeRepresentation}`;
 
   axios.get(requestURL)
     .then(result => {
-      res.status(200).send(result);
+      res.status(200).send(result.data);
     })
     .catch(err => {
-      console.log(err);
       res.status(400).send(err);
-    })
+    });
 
 }
 
