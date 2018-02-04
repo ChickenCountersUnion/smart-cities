6d544c2226c927cd68d9f50872ee9e6856e212d2
kiyeonpark <kiyeon.c.park@gmail.com>
2018-02-04 11:26:16 -0800

    finish functionality to set state, waiting for server side code

--- src/client/src/components/PickupForm.jsx
+++ src/client/src/components/PickupForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
-import { Button, Checkbox, Form, Dropdown } from 'semantic-ui-react'; 
+import { Button, Checkbox, Form, Dropdown } from 'semantic-ui-react';import SearchInput from './SearchInput'; 
 
 class PickupForm extends React.Component {
   constructor(props) {
@@ -10,16 +10,29 @@ class PickupForm extends React.Component {
       email: '', 
       address: '', 
       phone: '', 
-      type: [
+      type: [],
+      address: {}, 
 
-      ]
+
     }
     this.addEmail = this.addEmail.bind(this); 
     this.addPhone = this.addPhone.bind(this);
     this.toggleType = this.toggleType.bind(this); 
     this.sendUser = this.sendUser.bind(this); 
+    this.handleResultSelect = this.handleResultSelect.bind(this);
   }
 
+  handleResultSelect = (e, { result }) => {
+    let address = {
+      title: result.title, 
+      lat: result.lat, 
+      lon: result.lon
+    }; 
+    this.setState({
+       address
+    })
+
+  }
   addEmail = (e) => {
     this.setState({
       email: e.target.value
@@ -31,15 +44,24 @@ class PickupForm extends React.Component {
     })
   }
   toggleType = (e) => {
-
+    let type = this.state.type.slice();
+    type.push(e.target.value)
+    this.setState({
+      type
+    })
   }
   sendUser = () => {
     console.log(this.state); 
   }
   render() {
+    const { value } = this.state.type
     return (
       <div id="formbox">
+      <SearchInput 
+      handleResultSelect={this.handleResultSelect} 
+      />
         <Form>
+        
           <Form.Group widths='equal'>
             <Form.Input 
               fluid 
@@ -54,9 +76,21 @@ class PickupForm extends React.Component {
           </Form.Group>
           <Form.Group inline>
             <label>Type</label>
-            <Form.Checkbox label="Compost" checked={this.toggleType}/>
-            <Form.Checkbox label="Landfill" checked={this.toggleType}/>
-            <Form.Checkbox label="Recycle" checked={this.toggleType}/>
+            <Form.Checkbox label="Compost" value="compost"
+            control="input"
+            type="checkbox"
+            onChange={this.toggleType}
+            />
+            <Form.Checkbox label="Landfill" value="landfill"
+            control="input"
+            type="checkbox"
+            onChange={this.toggleType}
+            />
+            <Form.Checkbox label="Recycle" value="recycle"
+            control="input"
+            type="checkbox"
+            onChange={this.toggleType}
+            />
           </Form.Group>
   
           <Button fluid 
